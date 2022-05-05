3ab9f9658a5c1cb5a42cd092a5b7f0b7c2da2af5
mig-bn <ing.miguel.bn@gmail.com>
2022-05-05 10:58:43 -0500

    commit
    muestra id

--- models/user.go
+++ models/user.go
@@ -2,16 +2,17 @@ package models
 
 import (
 	"time"
-	//"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 //User: Datos del usuario
 type User struct {
-	//ID        primitive.ObjectID `bson:"_id,omitempty" json:"ID, omitempty"`
-	Name      string    `json:"Name"`
-	Email     string    `json:"Email"`
-	CreatedAt time.Time `bson:"created_At" json:"CreateAt"`
-	UpdatedAt time.Time `bson:"updated_At" json:"UpdatedAt,omitempty"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"ID, omitempty"`
+	Name      string             `json:"Name"`
+	Email     string             `json:"Email"`
+	CreatedAt time.Time          `bson:"created_At" json:"CreateAt"`
+	UpdatedAt time.Time          `bson:"updated_At" json:"UpdatedAt,omitempty"`
 }
 
 //Users: Lista de usuarios
